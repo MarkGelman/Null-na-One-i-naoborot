0af015228f41107c2409e1a1e7505c95752995ac
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-02 16:14:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zamena_simvolov_odin_na_odin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string st = "";
+            int len = str.Length;
+            for (int i=0; i<len; i++)
+            {
+                if (str[i] == '0')
+                    st = st + "1";
+                else
+                    if (str[i] == '1')
+                             st = st + "0";
+                else
+                   st += str[i];
+            }
+            Console.WriteLine(st);
+        }
+    }
+}
